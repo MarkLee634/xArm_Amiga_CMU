a500a465ece7051dbc4b5eed0a8d87cb5d5bbec7
marklee <lmy0510@gmail.com>
2023-04-04 13:42:20 -0400

    ROS service issue resolved. Working python script

--- README.md
+++ README.md
@@ -9,14 +9,16 @@
 `cd ~/catkin_ws/src/xArm_Amiga_CMU/xarm_gazebo/models` 
 `cp -dir amiga/ ~/.gazebo/models/`
 
-# Running
+# Running without custom End-Effector
 1. Gazebo
 `roslaunch xarm_gazebo amiga_xarm6.launch`
+Error: No p gain specified for pid.  `Namespace: /xarm/gazebo_ros_control/pid_gains/joint is` expected and can be ignored.
 
 2. MoveIt & xArm_Planner
 `roslaunch xarm6_moveit_config xarm6_moveit_gazebo_plan.launch`
+Manually move the EE in MoveIt GUI and press `Plan & Execute` to move the robot. Both RViz and Gazebo robot arm should move.
 
-3. Python Script for desired Motion 
+3. Python Script for desired Motion using ROS service to MoveGroup Interface
 (if first time, make this python script executable)
 `~/catkin_ws/src/xArm_Amiga_CMU/xarm_planner/scripts`
 `chmod +x xArm_planar_motion.py`
--- xarm_planner/scripts/xArm_planar_motion.py
+++ xarm_planner/scripts/xArm_planar_motion.py
@@ -17,103 +17,85 @@ Date:
 - Apr 2023.
 """
 
+HOME_POSE = [0.207, 0.0, 0.112, 0., 0., 0., 1.] 
+HOME_POSE_WITH_ROT = [0.207, 0.0, 0.112, 0.706675, 0., 0., 0.70754]
+MID_PT_POSE_TO_APPROACH = [0.293, 0.0, 0.67, 0.706675, 0., 0., 0.70754]
+APPROACH_POSE = [0.293, 0.05, 0.67, 0.706675, 0., 0., 0.70754]
+INSERT_POSE = [0.293, -0.25, 0.67, 0.706675, 0., 0., 0.70754]
 
+
+def get_desired_pose_from_list(input_list):
+
+    x = input_list[0]
+    y = input_list[1]
+    z = input_list[2]
+    qx = input_list[3]
+    qy = input_list[4]
+    qz = input_list[5]
+    qw = input_list[6]
+
+    return x, y, z, qx, qy, qz, qw
+
+def corn_stalk_motion():
+    #default home pose
+    x,y,z,qx,qy,qz,qw = get_desired_pose_from_list(HOME_POSE)
+
+
+    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
+    print(f"requesting plan: GO_HOME_POSE, Completed True/False?: {request_states}")
+    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
+    print(f"requesting exeuction: GO_HOME_POSE, Completed True/False?: {request_states}")
+
+    #default home pose w rotation
+    x,y,z,qx,qy,qz,qw = get_desired_pose_from_list(HOME_POSE_WITH_ROT)
+
+    
+    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
+    print(f"requesting plan: HOME_POSE_WITH_ROT, Completed True/False?: {request_states}")
+    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
+    print(f"requesting exeuction: HOME_POSE_WITH_ROT, Completed True/False?: {request_states}")
+
+    #Mid pt to approach pose for higher planning success rate
+    x,y,z,qx,qy,qz,qw = get_desired_pose_from_list(MID_PT_POSE_TO_APPROACH)
+
+    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
+    print(f"requesting plan: MID_PT_POSE_TO_APPROACH, Completed True/False?: {request_states}")
+    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
+    print(f"requesting exeuction: MID_PT_POSE_TO_APPROACH, Completed True/False?: {request_states}")
+
+    #Approach pose before inserting sensor into corn stalk
+    x,y,z,qx,qy,qz,qw = get_desired_pose_from_list(APPROACH_POSE)
+    
+
+    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
+    print(f"requesting plan: APPROACH_CARTESIAN, Completed True/False?: {request_states}")
+    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
+    print(f"requesting exeuction: APPROACH_CARTESIAN, Completed True/False?: {request_states}")
+
+    #pose for inserting sensor into corn stalk
+    x,y,z,qx,qy,qz,qw = get_desired_pose_from_list(INSERT_POSE)
+   
+
+    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
+    print(f"requesting plan: INSERT_CARTESIAN, Completed True/False?: {request_states}")
+    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
+    print(f"requesting exeuction: INSERT_CARTESIAN, Completed True/False?: {request_states}")
 
 if __name__ == "__main__":
     print(" ================ start ============ ")
 
     #obtained from manually playing with MoveIt and then reading joint postions
-    STOW_JOINT_ANGLES = [-0.005691000155291448, -1.5617350979917042, -3.817223679725146e-05, -0.0002003826846523893, -0.00011472079979313321, 3.616551335650087e-05]
-    APPROACH_JOINT_ANGLES = [-0.02991102667951928, -0.311088608000313, -1.189797271512119, 1.6267551770805806, 1.47070589650495, -0.0957004890775206]
+    # STOW_JOINT_ANGLES = [-0.005691000155291448, -1.5617350979917042, -3.817223679725146e-05, -0.0002003826846523893, -0.00011472079979313321, 3.616551335650087e-05]
+    # APPROACH_JOINT_ANGLES = [-0.02991102667951928, -0.311088608000313, -1.189797271512119, 1.6267551770805806, 1.47070589650495, -0.0957004890775206]
 
     #create wrapper object
     xArm_srv_wrapper = xArm_srv_wrapper.Srv2Python()
 
-    #move to stow position
-    # request_states = xArm_srv_wrapper.request_srv_joint_plan(STOW_JOINT_ANGLES)
-    # print(f"requesting plan: STOW_JOINT_ANGLES, Completed True/False?: {request_states}")
-    # request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
-    # print(f"requesting exeuction: STOW_JOINT_ANGLES, Completed True/False?: {request_states}")
-
-    # #move to approach position
-    # request_states = xArm_srv_wrapper.request_srv_joint_plan(APPROACH_JOINT_ANGLES)
-    # print(f"requesting plan: APPROACH_JOINT_ANGLES, Completed True/False?: {request_states}")
-    # request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
-    # print(f"requesting exeuction: STOW_JOINT_ANGLES, Completed True/False?: {request_states}")
-
-    #move to approach position in cartesian space
-    # x = 0.4
-    # y = 0.07
-    # z = 0.606
-    # qx = -0.68864
-    # qy = 0.039
-    # qz = -0.0557
-    # qw = 0.7219
-
-    #default home pose
-    x = 0.207
-    y = 0.0
-    z = 0.112
-    qx = 0.
-    qy = 0.
-    qz = 0.
-    qw = 1.
-
-    
-
-    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
-    print(f"requesting plan: APPROACH_CARTESIAN, Completed True/False?: {request_states}")
-    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
-    print(f"requesting exeuction: APPROACH_CARTESIAN, Completed True/False?: {request_states}")
-
-    #default home pose
-    x = 0.207
-    y = 0.0
-    z = 0.112
-    qx = 0.706675
-    qy = 0.
-    qz = 0.
-    qw = 0.70754
-
-    
-
-    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
-    print(f"requesting plan: APPROACH_CARTESIAN, Completed True/False?: {request_states}")
-    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
-    print(f"requesting exeuction: APPROACH_CARTESIAN, Completed True/False?: {request_states}")
-
-    #mid pt to approach
-    x = 0.293
-    y = 0.0
-    z = 0.67
-    qx = 0.706675
-    qy = 0.
-    qz = 0.
-    qw = 0.70754
+    #execute scripted motion for corn stalk insertion
+    corn_stalk_motion()
+    print(" ================ completed script ============ ")
 
 
     
 
-    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
-    print(f"requesting plan: APPROACH_CARTESIAN, Completed True/False?: {request_states}")
-    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
-    print(f"requesting exeuction: APPROACH_CARTESIAN, Completed True/False?: {request_states}")
 
-
-    # #approach cartesian pose
-    x = 0.293
-    y = 0.05
-    z = 0.67
-    qx = 0.706675
-    qy = 0.
-    qz = 0.
-    qw = 0.70754
-
-    request_states = xArm_srv_wrapper.request_srv_cartesian_pose_plan(x,y,z, qx,qy,qz,qw)
-    print(f"requesting plan: APPROACH_CARTESIAN, Completed True/False?: {request_states}")
-    request_states = xArm_srv_wrapper.request_srv_execute_plan(True)
-    print(f"requesting exeuction: APPROACH_CARTESIAN, Completed True/False?: {request_states}")
-
-
-
-    # request_srv_cartesian_plan(0.3, 0,0, 1,0,0,0)
